fa27425097f3ea9a4528ae023accea6f51c71217
ddkangfu <ddkangfu@gmail.com>
2014-11-28 23:21:18 +0800

    修改了计算数值不正确的问题。

--- convertr.py
+++ convertr.py
@@ -1,24 +1,26 @@
 #coding=utf-8
 
-
 def string_to_long(s):
     if not s:
         return 0 #这里需要返回非法格式异常
-    if not (s[0] == '+' or s[0] == '-' or (s[0] >=ord('0') and  s[0] <= ord('9'))):
+    if not (s[0] == '+' or s[0] == '-' or (ord(s[0]) >=ord('0') and ord(s[0]) <= ord('9'))):
         return 0 #这里需要返回非法格式异常
-    result = 1
+    result = 0
+    sign = 1
     for c in s:
         if c == '+':
             continue
         elif c == '-':
-            result *= -1
+            sign = -1
             continue
-        elif c >= ord('0') and c<= ord('9'):
+        elif ord(c) >= ord('0') and ord(c)<= ord('9'):
             result = result * 10 + (ord(c) - ord('0'))
         else:
             return 0
-    return result
+    return result * sign
 
 if __name__ == '__main__':
     print string_to_long('1234567')
+    print string_to_long('-5678')
+    print string_to_long('+1189700')
 
