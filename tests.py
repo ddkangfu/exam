84ee82ca7aa1c5d3d360e62c44c74803bdc71b3e
ddkangfu <ddkangfu@gmail.com>
2014-11-28 23:29:49 +0800

    增加了converter的单元测试类

---
+++ tests.py
@@ -0,0 +1,15 @@
+#coding=utf-8
+
+
+import unittest
+import converter import string_to_long
+
+class ConverterTest(unittest.TestCase):
+    def test_number_string_without_sign(self):
+        expect = 123456
+        result = string_to_long('123456')
+        self.assertEqual(result, expect)
+
+
+if __name__ == '__main__':
+    unittest.main()
