07a3665f5bbcaf18787afe087ca33d481cccdd85
ddkangfu <ddkangfu@gmail.com>
2014-11-29 22:55:40 +0800

    重构了string_to_long函数，使其更加pythonic.

--- converter.py
+++ converter.py
@@ -1,8 +1,12 @@
 #coding=utf-8
 
 import re
+import math
+
 
 def string_to_long(s):
+    s = s.strip()
+
     if not s:
         return 0 #这里需要返回非法格式异常
 
@@ -10,20 +14,10 @@ def string_to_long(s):
     match = pattern.match(s)
     if not match:
         return 0 #这里需要返回非法格式异常
-    
-    result = 0
-    sign = 1
-    for c in s:
-        if c == '+':
-            continue
-        elif c == '-':
-            sign = -1
-            continue
-        elif ord(c) >= ord('0') and ord(c)<= ord('9'):
-            result = result * 10 + (ord(c) - ord('0'))
-        else:
-            return 0
-    return result * sign
+
+    rev_s = s[::-1]
+    result = sum([int(math.pow(10, i)) * (ord(rev_s[i]) - ord('0')) for i in range(len(rev_s)) if rev_s[i] not in ['+', '-']])
+    return (result * -1) if s[0] == '-' else result
 
 
 if __name__ == '__main__':
