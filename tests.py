380afbde4085e03485f5c442c060e197b49b348b
ddkangfu <ddkangfu@gmail.com>
2014-11-29 23:09:56 +0800

    增加了0、正负号数字的测试用例。

--- tests.py
+++ tests.py
@@ -10,6 +10,21 @@ class ConverterTest(unittest.TestCase):
         result = string_to_long('123456')
         self.assertEqual(result, expect)
 
+    def test_number_string_equal_zero(self):
+        expect = 0
+        result = string_to_long('0')
+        self.assertEqual(result, expect)
+
+    def test_number_string_with_positive_sign(self):
+        expect = 123456
+        result = string_to_long('+123456')
+        self.assertEqual(result, expect)
+
+    def test_number_string_with_negative_sign(self):
+        expect = -654321
+        result = string_to_long('-654321')
+        self.assertEqual(result, expect)        
+
 
 if __name__ == '__main__':
     unittest.main()
