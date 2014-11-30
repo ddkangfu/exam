3c50233e1317ac588fd53977df5194910e31327b
ddkangfu <ddkangfu@gmail.com>
2014-11-30 20:52:36 +0800

    增加了TreeNode类和TriNaryTree类。

---
+++ tri_nary_tree.py
@@ -0,0 +1,23 @@
+#coding=utf-8
+
+
+class TreeNode(object):
+    def __init__(self, value, left=None, middle=None, right=None):
+        self.value = value
+        self.left = left
+        self.middle = middle
+        self.right = right
+
+
+class TriNaryTree(object):
+    def __init__(self):
+        self.root = None
+
+    def _search(self, value):
+        pass
+
+    def insert(self, value):
+        pass
+
+    def delete(self, value):
+        pass
